c8a4d37f7a8b071a4accdcb5ebbf698af409e3d0
matHuston <thecaranar@gmail.com>
2026-01-28 11:03:52 -0500

    public class done

--- week4/ArrayModuleDemo.java
+++ week4/ArrayModuleDemo.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Arrays;
  
  
 /*
@@ -15,60 +14,91 @@ import java.util.Arrays;
     - Variable-length arguments
     - ArrayList introduction
 */
+/*
+    VarArgs method
+    - can have variable amount of values
+    - VarArgs(type...values)
+*/
  
  
  
  
 // STEP 1: Utility class for array operations
 class ArrayUtilities {
- 
- 
-    // Method to calculate sum of array elements
+
+    // Method to calculate sum of array elements - method name is sumArray, in params we are saying this is accepting an int array - you can pass arrays as params to methods
     public static int sumArray(int[] numbers) {
-        // TODO: use loop to calculate sum
-        return 0;
+        // TO DO: use a loop to calculate sum
+        int sum = 0;
+        for(int num : numbers){
+            sum += num;
+        }
+        return sum;
     }
  
- 
-    // Method to find average
+    // Method to find average - make sure to check return value, in this case is a double which means we need to return it as double
     public static double averageArray(int[] numbers) {
-        // TODO: call sumArray and compute average
-        return 0.0;
+        // TO DO: call sumArray and compute average
+        int sum = sumArray(numbers);
+        return (double) sum / numbers.length; // this is how to convert to double
     }
  
- 
-    // Variable-length argument method
+    // Variable-length argument method (Varargs) - allows passing any number of values
     public static int maxValue(int... values) {
-        // TODO: find maximum value
-        return 0;
+        // TO DO: find maximum value
+        int max = values[0]; // initialize max with first values index
+        for(int val : values){
+            if(val > max){
+                max = val; // update max whenever a value bigger than the max is found
+            }
+        }
+        return max;
     }
 }
- 
- 
- 
  
 // STEP 2: Class to demonstrate 2D arrays
 class GradeBook {
  
- 
-    private int[][] grades;
- 
- 
-    // Constructor
+    private int[][] grades; // declare with [][]
+  
+    // Constructor -- initializing the value of instanced var grades
     public GradeBook(int[][] gradesArray) {
-        // TODO: assign grades
+        // TO DO: assign grades
+        this.grades = gradesArray;
     }
- 
  
     // Display grades
     public void displayGrades() {
-        // TODO: nested loop to print 2D array
+        // TO DO: nested loop to print 2D array
+        System.out.println("\nStudent Grades: ");
+        //first loop - first array is grades, this loop sets up the rows in the multiarray
+        for(int i = 0; i < grades.length; i++){
+            System.out.print("Student: " + i + 1 + ":");
+            //second loop - this array is for populating each row because of passing i through grades[i].length, this goes through the grades of each student because each student is an i
+            for(int j = 0; j < grades[i].length; j++){
+                System.out.print(grades[i][j] + "");
+            }
+            System.out.println();
+            /*
+                student1 {10,20,30}
+                student2 {40,50,60}
+                student3 {70,80,90}
+            */
+        }
     }
- 
  
     // Calculate average for each student
     public void calculateAverages() {
-        // TODO: loop through rows and compute averages
+        // TO DO: loop through rows and compute averages
+        System.out.println("\nStudent Averages");
+        for(int i = 0; i <grades.length; i++){
+            int sum = 0;
+            for(int num : grades[i]){
+                sum += num;
+            }
+        }
+        double avg = (double) sum / grades[i].length;
+        System.out.println("Student: " + (i+1) + " Average: " + avg);
     }
 }
  
